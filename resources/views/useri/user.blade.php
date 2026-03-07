8468cc4f8645010c91b264803069cba66b3a80b3
yudi.hardiyanta <yudi.hardiyanta@bps.go.id>
2026-03-07 14:27:04 +0800

    perbaiki keluar di web

--- resources/views/useri/user.blade.php
+++ resources/views/useri/user.blade.php
@@ -82,7 +82,7 @@
         <div class="dropdown-menu bg-light mt-2">
             <a href="/profile" class="dropdown-item">Profile</a>
 
-            <form method="POST" action="{{ route('logout') }}">
+            <form method="GET" action="{{ route('keluar') }}">
                 @csrf
                 <button type="submit" class="dropdown-item">
                     Keluar
